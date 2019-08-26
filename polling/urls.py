925db45d3ef3e75ee525c3e7ea543cc075056365
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-08-26 14:00:08 +0430

    include account urls and change poll url pattern

--- polling/urls.py
+++ polling/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('poll/',include('poll.urls',namespace="poll")),
+    path('polling/',include('poll.urls',namespace="polling")),
+    path('accounts/',include('account.urls',namespace='accounts')),
 ]
