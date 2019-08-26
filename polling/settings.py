40ed5da92259cb7b7a58ae6aab48038aa2b002eb
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-08-26 13:59:31 +0430

    install account app to manage user accounts

--- polling/settings.py
+++ polling/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'poll',
     'rest_framework',
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
