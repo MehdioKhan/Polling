6060074192330fcb702c5169e8480691bec535fa
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-08-26 15:46:28 +0430

    login,logout and register views added

--- account/views.py
+++ account/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSignupSerializer,UserSerializer
+from django.utils import timezone
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 
-# Create your views here.
+
+class UserSignup(generics.CreateAPIView):
+    serializer_class = UserSignupSerializer
+
+
+@api_view(['POST'])
+def login(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data['user']
+    user.last_login = timezone.now()
+    user.save(update_fields=['last_login'])
+    token,created = Token.objects.get_or_create(user=user)
+    user_serializer = UserSerializer(user)
+    return Response({'token':token.key,'user':user_serializer.data})
+
+
+def delete_token(user):
+    try:
+        token = Token.objects.get(user=user)
+        token.delete()
+    except Token.DoesNotExist:
+        pass
+    return
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    delete_token(request.user)
+    return Response('Logged out')
