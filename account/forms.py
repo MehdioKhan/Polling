5025aa617a91fe952093a5481fdb5edcc0c9f3dc
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-08-27 15:07:43 +0430

    remove username from user model and usee email for authentication

---
+++ account/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = get_user_model()
+        fields = ('email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta(UserChangeForm):
+        model = get_user_model()
+        fields = ('email',)
--- account/serializers.py
+++ account/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class UserSignupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create(
-            username=validated_data['username'],
             email=validated_data['email'],
         )
         user.set_password(validated_data['password'])
@@ -23,4 +24,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id','username')
+        fields = ('id','email')
