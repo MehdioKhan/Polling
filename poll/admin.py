49185f4063c77fb0ea30c2c08d71d6493a415f0a
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-08-30 22:40:52 +0430

    add new model for url and add some fileds to other models

--- poll/admin.py
+++ poll/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Poll,Question,QuestionAnswer,Choice,PollAnswer
+from .models import Poll,Question,QuestionAnswer,Choice,PollAnswer,RequestedPoll
 
 
-admin.site.register([Poll,Question,Choice,QuestionAnswer,PollAnswer])
+admin.site.register([Poll,Question,Choice,QuestionAnswer,PollAnswer,RequestedPoll])
--- poll/models.py
+++ poll/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from account.models import User
 from django.core.exceptions import ValidationError
+from django.urls import reverse
+import uuid
 
 
 class Poll(models.Model):
@@ -34,29 +36,41 @@ class Choice(models.Model):
 class QuestionAnswer(models.Model):
     question = models.ForeignKey(to='Question',on_delete=models.CASCADE,
                                  related_name='answer')
-    selected = models.ForeignKey(to='Choice',on_delete=models.CASCADE)
-    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    answer = models.ForeignKey(to='Choice',on_delete=models.CASCADE)
+    from_user = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='answered_to_question')
+    to_user = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='question_answers')
     created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        unique_together = (('question','user'),)
-
     def __str__(self):
-        return "{} selected {} for {}".format(self.user,self.selected.text,self.question.body)
+        return "{} selected {} for {}".format(self.from_user,self.answer.text,self.question.body)
 
     def full_clean(self, exclude=None, validate_unique=True):
         valid_choices = self.question.choices.all()
-        if self.selected not in valid_choices:
+        if self.answer not in valid_choices:
             raise ValidationError(
-                'Selected choice "{}" is not one of the permitted values {}'.format(self.selected,valid_choices))
+                'Selected choice "{}" is not one of the permitted values {}'.format(self.answer,valid_choices))
         super(QuestionAnswer,self).full_clean(exclude=exclude, validate_unique=validate_unique)
 
 
 class PollAnswer(models.Model):
     poll = models.ForeignKey(to='Poll',on_delete=models.CASCADE)
     questions_answers = models.ManyToManyField(to='QuestionAnswer')
-    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    from_user = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='answered_to')
+    to_user = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='answers')
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "{} answered to {}".format(self.user,self.poll.name)
+        return "{} answered to {} for {}".format(self.from_user,self.poll.name,self.to_user)
+
+
+class RequestedPoll(models.Model):
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    poll = models.ForeignKey(to='Poll',on_delete=models.CASCADE,related_name='url')
+    url_param = models.CharField(max_length=72,default=uuid.uuid4().hex,editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (('user','poll'),)
+
+    def __str__(self):
+        return self.url_param
