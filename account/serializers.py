569dad807f27bb4f93fe1ce711a0223272bfc97e
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-09-03 16:43:17 +0430

    change login view

--- account/serializers.py
+++ account/serializers.py
@@ -1,55 +1,1 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth import authenticate
-
-User = get_user_model()
-
-
-class UserSignupSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ('email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            email=validated_data['email'],
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ('id','email')
-
-
-class UserAuthTokenSerializer(serializers.Serializer):
-    email = serializers.CharField(label=_("Email"))
-    password = serializers.CharField(
-        label=_("Password"),
-        style={'input_type': 'password'},
-        trim_whitespace=False
-    )
-
-    def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
-
-        if email and password:
-            user = authenticate(request=self.context.get('request'),
-                                email=email, password=password)
-            if not user:
-                msg = _('Unable to log in with provided credentials.')
-                raise serializers.ValidationError(msg, code='authorization')
-        else:
-            msg = _('Must include "email" and "password".')
-            raise serializers.ValidationError(msg, code='authorization')
-
-        attrs['user'] = user
-        return attrs
+from rest_framework import serializersfrom django.contrib.auth import get_user_modelfrom django.utils.translation import gettext_lazy as _User = get_user_model()class UserSignupSerializer(serializers.ModelSerializer):    class Meta:        model = User        fields = ('email', 'password')        extra_kwargs = {'password': {'write_only': True}}    def create(self, validated_data):        user = User.objects.create(            email=validated_data['email'],        )        user.set_password(validated_data['password'])        user.save()        return userclass UserSerializer(serializers.ModelSerializer):    class Meta:        model = User        fields = ('id','email')class UserSignInSerializer(serializers.Serializer):    email = serializers.CharField(label=_("Email"))    password = serializers.CharField(        label=_("Password"),        style={'input_type': 'password'},        trim_whitespace=False    )
--- account/views.py
+++ account/views.py
@@ -2,26 +2,47 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated,AllowAny
-from .serializers import UserSignupSerializer,UserSerializer,UserAuthTokenSerializer
-from django.utils import timezone
+from .serializers import UserSignupSerializer,UserSerializer,UserSignInSerializer
 from rest_framework.authtoken.models import Token
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK,
+)
+from .authentication import token_expire_handler, expires_in
+from django.contrib.auth import authenticate
 
 
 class UserSignup(generics.CreateAPIView):
     serializer_class = UserSignupSerializer
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny,])
+@api_view(["POST"])
+@permission_classes((AllowAny,))  # here we specify permission by default we set IsAuthenticated
 def login(request):
-    serializer = UserAuthTokenSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    user = serializer.validated_data['user']
-    user.last_login = timezone.now()
-    user.save(update_fields=['last_login'])
-    token,created = Token.objects.get_or_create(user=user)
-    user_serializer = UserSerializer(user)
-    return Response({'token':token.key,'user':user_serializer.data})
+    signin_serializer = UserSignInSerializer(data=request.data)
+    if not signin_serializer.is_valid():
+        return Response(signin_serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    user = authenticate(
+        email=signin_serializer.data['email'],
+        password=signin_serializer.data['password']
+    )
+    if not user:
+        return Response({'detail': 'Invalid Credentials or activate account'}, status=HTTP_404_NOT_FOUND)
+
+    # TOKEN STUFF
+    token, _ = Token.objects.get_or_create(user=user)
+
+    # token_expire_handler will check, if the token is expired it will generate new one
+    is_expired, token = token_expire_handler(token)  # The implementation will be described further
+    user_serialized = UserSerializer(user)
+
+    return Response({
+        'user': user_serialized.data,
+        'expires_in': expires_in(token),
+        'token': token.key
+    }, status=HTTP_200_OK)
 
 
 def delete_token(user):
