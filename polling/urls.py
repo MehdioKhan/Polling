574c36856b389b9d03593e0794853c9d8f524079
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-09-14 19:11:41 +0430

    undo last commit

--- polling/urls.py
+++ polling/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('poll.urls',namespace="polling")),
+    path('polling/',include('poll.urls',namespace="polling")),
     path('accounts/',include('account.urls',namespace='accounts')),
 ]
