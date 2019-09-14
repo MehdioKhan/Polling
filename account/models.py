b0af6198ab0fa3a410639164fd4d81c0c16cee79
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-09-14 20:44:52 +0430

    bugfix

--- account/models.py
+++ account/models.py
@@ -11,7 +11,7 @@ class UserManager(BaseUserManager):
         """Create and save a User with the given email and password."""
         if not email:
             raise ValueError('The given email must be set')
-        email = self.normalize_email(email.lower())
+        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
